6b353a3ed76ca6b5bdc201ee32f953ad5caabad2
AlexMancinas-atos <alex-antonio.mancinas@atos.net>
2022-07-26 16:59:31 -0700

    actualization

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,33 @@
 import axios from "axios";
-import * as tingle from 'tingle.js'
+import * as tingle from "tingle.js";
 
+let order = false;
 
 const url = "https://restcountries.com/v3.1/all";
 
-let paisp = document.getElementById('pais')!;
+let paisp = document.getElementById("pais")!;
+let paisesData: any;
 axios
   .get(url)
-  .then((data) =>  {
-    const paisesData = data.data
-    console.log(paisesData)
-    extractData(paisesData)})
+  .then((reponse) => {
+    paisesData = reponse.data as Array<any>; // * @ referenced array
+
+    sortArray(paisesData);
+    extractData(paisesData);
+  })
   .catch((err) => console.log(err));
 
+const extractData = (paisesData: any) => {
+  let body = "";
 
-  const extractData = (paisesData: any)=>{
-    let body ='';
+  for (let i = 0; i < paisesData.length; i++) {
+    let item: any;
 
+    const languages = Object.keys(paisesData[i].languages || {}).map(
+      (key) => paisesData[i].languages[key]
+    );
 
-
-
-
-    
-    for(let i = 0; i<paisesData.length ; i++){
-        let item: any;  
-      
-        const languages = Object.keys(paisesData[i].languages|| {}).map((key)=>paisesData[i].languages[key])
-        
-        body+= `<tr>
+    body += `<tr>
             
             <td onclick=()>${paisesData[i].name.official}</td>
             <td>${paisesData[i].capital}</td>
@@ -36,61 +36,37 @@ axios
             <td>${paisesData[i].population}</td>
             <td><img src="${paisesData[i].flags.png}"></td>
             
-        </tr>`
-       
-    }
-    
-    document.getElementById('data')!.innerHTML = body
-
+        </tr>`;
   }
 
+  document.getElementById("data")!.innerHTML = body;
+};
 
-  let reduceToString = function reduceToString(str_arr: string[] , default_message: string) {
-    const valid_array = (typeof str_arr === "object" && str_arr.length > 0);
-    return valid_array ? str_arr.reduce((f, l) => `${f}, ${l} `) : default_message;
+let reduceToString = function reduceToString(
+  str_arr: string[],
+  default_message: string
+) {
+  const valid_array = typeof str_arr === "object" && str_arr.length > 0;
+  return valid_array
+    ? str_arr.reduce((f, l) => `${f}, ${l} `)
+    : default_message;
+};
+
+const sortAsc = (a: any, b: any) =>
+  a.name.official.localeCompare(b.name.official);
+
+const sortDesc = (a: any, b: any) =>
+  b.name.official.localeCompare(a.name.official);
+
+function sortArray(paisesData: Array<any>) {
+  paisesData.sort(sortAsc);
 }
 
-function sort(){
-  
-}
+const sort = ($e:Event) => {
+  $e.preventDefault();
+  paisesData.sort(order ? sortAsc : sortDesc);
+  extractData(paisesData);
+  order = !order;
+};
 
-var modal = new tingle.modal({
-  footer: true,
-  stickyFooter: false,
-  closeMethods: ['overlay', 'button', 'escape'],
-  closeLabel: "Close",
-  cssClass: ['custom-class-1', 'custom-class-2'],
-  onOpen: function() {
-      console.log('modal open');
-  },
-  onClose: function() {
-      console.log('modal closed');
-  },
-  beforeClose: function() {
-      // here's goes some logic
-      // e.g. save content before closing the modal
-      return true; // close the modal
-      return false; // nothing happens
-  }
-});
-
-// set content
-modal.setContent('<h1>here\'s some content</h1>');
-
-// add a button
-modal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
-  // here goes some logic
-  modal.close();
-});
-
-// add another button
-modal.addFooterBtn('Dangerous action !', 'tingle-btn tingle-btn--danger', function() {
-  // here goes some logic
-  modal.close();
-});
-
-// open modal
-modal.open();
-
-// close modal
-modal.close();
+document.getElementById('orderArray')?.addEventListener('click',sort);
